84955ff4f73a9dc332aae8248bc17ae588f83a77
Maya Kappen <mayakappen@gmail.com>
2022-09-07 11:09:39 -0600

    Add trick component

--- src/components/App/Trick.js
+++ src/components/App/Trick.js
@@ -1,5 +1,15 @@
 import React from 'react'
 
 class Trick extends Component {
+    constructor(props) {
+        super(props) 
+            this.state = {
+                stance: props.stance,
+                name: props.name,
+                obstacle: props.obstacle,
+                tutorial: props.tutorial,
+                id: props.id
+            }
+    }
     
 }
