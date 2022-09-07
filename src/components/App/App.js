385341bb1a549332c0ad08a9304a9cb8f30f1753
Maya Kappen <mayakappen@gmail.com>
2022-09-07 09:45:27 -0600

    Add fetch method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,18 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+      state = {
+      tricks: []
+    }
+  fetchTricks() {
+    fetch('http://localhost:3001/api/v1/tricks')
+      .then(resp => resp.json())
+      .then((obj) => {
+        this.setState({
+          tricks: obj
+        })
+      })
+    }
   render() {
     return (
       <div className="App">
